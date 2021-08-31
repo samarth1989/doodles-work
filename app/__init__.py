4e203ed1f4c3c6dbc387b088a2104cab74e3ef5f
samarth <30378006+samarth1989@users.noreply.github.com>
2021-08-31 11:05:02 +0100

    All code at once

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.config.from_object(Config)
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+from app import routes, models
---
+++ app/routes.py
@@ -0,0 +1,63 @@
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
+import os
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    path = 'static/Legos'
+    list_of_files = []
+
+    for root, dirs, files in os.walk('./app/static/Legos'):
+	    for file in files:
+		    list_of_files.append(os.path.join(path,file))
+    # for name in list_of_files:
+    #     print(name)
+
+    return render_template('index.html', title='Home', user=user, posts=posts, images = list_of_files)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html',  title='Sign In', form=form)
+
+
+@app.route('/<item>', methods=['GET'])
+def show(item):
+    path = 'static/Legos'
+    image = os.path.join(path,item)
+    return render_template('showimage.html',image = image)
+
+
+@app.route('/minecraft')
+def minecraft():
+    
+    path = 'static/Legos'
+    list_of_files = []
+
+    for root, dirs, files in os.walk('./app/static/Legos'):
+	    for file in files:
+		    list_of_files.append(os.path.join(path,file))
+    # for name in list_of_files:
+    #     print(name)
+
+    return render_template('index.html', title='Home', images = list_of_files)
